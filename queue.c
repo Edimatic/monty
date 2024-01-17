7f99b2263e784869822e650fa5bd68f2e9007d6a
Edimatic <ediomiudofia@gmail.com>
2024-01-17 15:31:59 -0800

    Stacks, Queues - LIFO, FIFO

--- queue.c
+++ queue.c
@@ -28,4 +28,15 @@ void q_queue_(stack_t **stack, unsigned int line_nums)
 
 	temp->next->next = NULL;
 }
+/**
+ * stack - sets the format of the data to a stack (LIFO)
+ * @stack: double pointer to the head of the stack
+ * @line_number: current line number in the file
+ */
+void s_stack_(stack_t **stack, unsigned int line_nums)
+{
+    (void)line_nums;
+
+    (void)stack; /* No action needed for stack mode */
+}
 
